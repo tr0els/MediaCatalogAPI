afe5122f8f29ef6e1113591ec5ba7ac2eaa23a99
Troels Klein <trk@trifork.com>
2023-03-28 13:50:48 +0200

    Renamed folder

---
+++ MediaCatalog.RestApi/Controllers/CatalogController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using MediaCatalog.Core.Entities;
+using MediaCatalog.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace MediaCatalog.RestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : Controller
+    {
+        private IRepository<Catalog> catalogRepository;
+        private IProductRepository<Product> productRepository;
+        private IRepository<Image> imageRepository;
+        private IRepository<ImageVariant> imageVariantRepository;
+        private ICatalogManager catalogManager;
+
+        public CatalogController(
+            IRepository<Catalog> catalogRepos,
+            IProductRepository<Product> productRepos,
+            IRepository<Image> imageRepos,
+            IRepository<ImageVariant> imageVariantRepos,
+            ICatalogManager manager)
+        {
+            catalogRepository = catalogRepos;
+            productRepository = productRepos;
+            imageRepository = imageRepos;
+            imageVariantRepository = imageVariantRepos;
+            catalogManager = manager;
+        }
+
+        // GET: catalogs
+        [HttpGet(Name = "GetCatalogs")]
+        public IEnumerable<Catalog> Get()
+        {
+            return catalogManager.GetAllCatalogs();
+        }
+
+        // GET catalogs/5
+        [HttpGet("{id}", Name = "GetCatalog")]
+        public IActionResult Get(int id)
+        {
+            var item = catalogRepository.Get(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        // POST catalog
+        [HttpPost]
+        public IActionResult Post([FromBody] Catalog catalog)
+        {
+            if (catalog == null)
+            {
+                return BadRequest();
+            }
+
+            try {
+                var createdCatalog = catalogManager.CreateCatalog(catalog);
+                return CreatedAtRoute("GetCatalogs", createdCatalog);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT catalog/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Catalog catalog)
+        {
+            try {
+                var updatedCatalog = catalogRepository.Edit(catalog);
+                return new ObjectResult(updatedCatalog);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE catalogs/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var deletedCatalog = catalogRepository.Remove(id);
+                return new ObjectResult(deletedCatalog);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ MediaCatalog.RestApi/Controllers/ImageController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using MediaCatalog.Core.Entities;
+using MediaCatalog.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using MediaCatalog.Core.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace MediaCatalog.RestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageController : Controller
+    {
+        private IRepository<Catalog> catalogRepository;
+        private IProductRepository<Product> productRepository;
+        private IRepository<Image> imageRepository;
+        private IRepository<ImageVariant> imageVariantRepository;
+        private ICatalogManager catalogManager;
+        private IProductManager productManager;
+
+        public ImageController(
+            IRepository<Catalog> catalogRepos,
+            IProductRepository<Product> productRepos,
+            IRepository<Image> imageRepos,
+            IRepository<ImageVariant> imageVariantRepos,
+            ICatalogManager catManager,
+            IProductManager prodManager)
+        {
+            catalogRepository = catalogRepos;
+            productRepository = productRepos;
+            imageRepository = imageRepos;
+            imageVariantRepository = imageVariantRepos;
+            catalogManager = catManager;
+            productManager = prodManager;
+        }
+
+        // GET: images
+        [HttpGet(Name = "GetImages")]
+        public IEnumerable<Image> Get()
+        {
+            return imageRepository.GetAll();
+        }
+
+        // GET image/5
+        [HttpGet("{id}", Name = "GetImage")]
+        public IActionResult Get(int id)
+        {
+            var item = imageRepository.Get(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        // POST image
+        [HttpPost]
+        public IActionResult Post([FromBody] Image image)
+        {
+            try
+            {
+                var createdImage = productManager.AddImageToProduct(image);
+                return new ObjectResult(createdImage);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT image/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Image image)
+        {
+            if (image == null || image.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var modifiedImage = imageRepository.Get(id);
+
+            if (modifiedImage == null)
+            {
+                return NotFound();
+            }
+
+            imageRepository.Edit(modifiedImage);
+            return NoContent();
+        }
+
+        // DELETE image/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (imageRepository.Get(id) == null)
+            {
+                return NotFound();
+            }
+
+            imageRepository.Remove(id);
+            return NoContent();
+        }
+    }
+}
